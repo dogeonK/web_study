1d06ef4ffc1dc2c19f62bc444438dbb46c964a83
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-05 22:54:34 +0900

    [UPDATE] npm example

--- nodeJS_study/index.js
+++ nodeJS_study/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+var figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
